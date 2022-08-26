b1364a3aff2ef0076ea74aa5719d3f2442d49108
Davin <davin-dj@hotmail.com>
2022-08-26 16:52:52 -0400

    Completed Activity.

--- move.js
+++ move.js
@@ -6,10 +6,10 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
-    function moveWithArrowKeys(left, bottom) {
+    function moveWithArrowKeys(left, bottom, callback) {
         let direction = null
-        let x = 100
-        let y = 250
+        let x = left
+        let y = bottom
 
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
@@ -46,10 +46,12 @@ function move(element) {
             if (e.key === 'ArrowDown') {
                 direction = 'south'
             }
+            callback(direction)
         })
 
         document.addEventListener('keyup', function (e) {
             direction = null
+            callback(direction)
         })
     }
 
